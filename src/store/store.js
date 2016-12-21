3a1bf87112b775fe5adb9f5dda62f1b545541832
Rainer Kuhn <ticktricktrack@gmail.com>
2016-12-21 01:52:02 +0100

    default to berlin if not in DE

--- src/store/store.js
+++ src/store/store.js
@@ -3,6 +3,10 @@ import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
+const GOOGLE_GEO = 'AIzaSyB0ZPdrd_zcZKXJjM0oTXsJlCM1UI-A4Cs'
+const GOOGLE_TZ = 'AIzaSyAaXjpRzDBNwz5mka52VXjUJK_CZEGF1Bo'
+const TANKER = '45caee49-4410-1536-20ee-509b9fc223e7'
+
 export default new Vuex.Store({
   state: {
     selected: null,
@@ -45,7 +49,7 @@ export default new Vuex.Store({
 
     loadStations({getters, commit}, {lat, lng}) {
       Vue.http.get(
-        `https://creativecommons.tankerkoenig.de/json/list.php?lat=${lat}&lng=${lng}&rad=4&sort=price&type=diesel&apikey=45caee49-4410-1536-20ee-509b9fc223e7`,
+        `https://creativecommons.tankerkoenig.de/json/list.php?lat=${lat}&lng=${lng}&rad=4&sort=price&type=diesel&apikey=${TANKER}`,
       )
       .then(response => response.json())
       .then(data => {
@@ -62,11 +66,27 @@ export default new Vuex.Store({
     },
 
     currentLocation({ commit, dispatch }) {
-      Vue.http.post('https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyB0ZPdrd_zcZKXJjM0oTXsJlCM1UI-A4Cs')
+      Vue.http.post(`https://www.googleapis.com/geolocation/v1/geolocate?key=${GOOGLE_GEO}`)
       .then(response => response.json())
       .then(data => {
         commit("coords", data.location);
-        dispatch("loadStations", data.location);
+        dispatch("checkCountry", data.location);
+      });
+    },
+
+    checkCountry({ commit, dispatch}, coords) {
+      Vue.http.get(`https://maps.googleapis.com/maps/api/timezone/json?location=${coords.lat},${coords.lng}&timestamp=1458000000&key=${GOOGLE_TZ}`)
+      .then(response => response.json())
+      .then(data => {
+        if (data.timeZoneId != "Europe/Berlin") {
+          coords = {
+            lat: 52.52,
+            lng: 13.4
+          }
+        }
+        console.log("cooords", coords);
+        commit("coords", coords);
+        dispatch("loadStations", coords);
       });
     },
 
